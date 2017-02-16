c8f557c7deda8797bb01ebfb04f6f976603a5563
Ebram Kerolos <er86@njit.edu>
2017-02-15 20:09:35 -0500

    adding files

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+    $dsn = 'mysql:host=sql2.njit.edu;dbname=er86';
+    $username = 'er86';
+    $password = 'MMA6gsP3';
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('database_error.php');
+        exit();
+    }
+?>
