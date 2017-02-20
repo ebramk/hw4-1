434f3aaa454254210ea5d4e38a6ca42e77f0528d
Ebram Kerolos <er86@njit.edu>
2017-02-20 18:14:31 -0500

    added files after doing hw

--- category_list.php
+++ category_list.php
@@ -1,16 +1,13 @@
 <?php
-require_once('database.php');
-
-// Get all categories
-$query = 'SELECT * FROM categories
-                       ORDER BY categoryID';
-$statement = $db->prepare($query);
-$statement->execute();
-$categories = $statement->fetchAll();
-$statement->closeCursor();
+    require_once('database.php');
+    // Get all categories
+    $query = 'SELECT * FROM categories
+              ORDER BY categoryID';
+    $categories = $db->query($query);
 ?>
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
 <!-- the head section -->
 <head>
@@ -20,30 +17,55 @@ $statement->closeCursor();
 
 <!-- the body section -->
 <body>
-<header><h1>Product Manager</h1></header>
-<main>
+    <div id="page">
+
+    <div id="header">
+        <h1>Product Manager</h1>
+    </div>
+
+    <div id="main">
+
     <h1>Category List</h1>
     <table>
         <tr>
             <th>Name</th>
             <th>&nbsp;</th>
         </tr>
-        
-        <!-- add code for the rest of the table here -->
-    
+        <?php foreach ($categories as $category) : ?>
+        <tr>
+            <td><?php echo $category['categoryName']; ?></td>
+            <td>
+                <form action="delete_category.php" method="post"
+                      id="delete_product_form">
+                    <input type="hidden" name="category_id"
+                           value="<?php echo $category['categoryID']; ?>"/>
+                    <input type="submit" value="Delete"/>
+                </form>
+            </td>
+        </tr>
+        <?php endforeach; ?>
     </table>
+    <br />
 
     <h2>Add Category</h2>
-    
-    <!-- add code for the form here -->
-    
-    <br>
+    <form action="add_category.php" method="post"
+          id="add_category_form">
+
+        <label>Name:</label>
+        <input type="input" name="name" />
+        <input id="add_category_button" type="submit" value="Add"/>
+    </form>
+    <br />
     <p><a href="index.php">List Products</a></p>
 
-    </main>
+    </div> <!-- end main -->
 
-    <footer>
-        <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
-    </footer>
+    <div id="footer">
+        <p>
+            &copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.
+        </p>
+    </div>
+
+    </div><!-- end page -->
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,43 +1,4 @@
-<?php
-require_once('database.php');
 
-// Get category ID
-if (!isset($category_id)) {
-    $category_id = filter_input(INPUT_GET, 'category_id', 
-            FILTER_VALIDATE_INT);
-    if ($category_id == NULL || $category_id == FALSE) {
-        $category_id = 1;
-    }
-}
-// Get name for selected category
-$queryCategory = 'SELECT * FROM categories
-                  WHERE categoryID = :category_id';
-$statement1 = $db->prepare($queryCategory);
-$statement1->bindValue(':category_id', $category_id);
-$statement1->execute();
-$category = $statement1->fetch();
-$category_name = $category['categoryName'];
-$statement1->closeCursor();
-
-
-// Get all categories
-$query = 'SELECT * FROM categories
-                       ORDER BY categoryID';
-$statement = $db->prepare($query);
-$statement->execute();
-$categories = $statement->fetchAll();
-$statement->closeCursor();
-
-// Get products for selected category
-$queryProducts = 'SELECT * FROM products
-                  WHERE categoryID = :category_id
-                  ORDER BY productID';
-$statement3 = $db->prepare($queryProducts);
-$statement3->bindValue(':category_id', $category_id);
-$statement3->execute();
-$products = $statement3->fetchAll();
-$statement3->closeCursor();
-?>
 <!DOCTYPE html>
 <html>
 
@@ -58,19 +19,22 @@ $statement3->closeCursor();
         <h2>Categories</h2>
         <nav>
         <ul>
-            <?php foreach ($categories as $category) : ?>
-            <li><a href=".?category_id=<?php echo $category['categoryID']; ?>">
-                    <?php echo $category['categoryName']; ?>
-                </a>
+                        <li><a href=".?category_id=1">
+                    Guitars                </a>
             </li>
-            <?php endforeach; ?>
-        </ul>
+                        <li><a href=".?category_id=2">
+                    Basses                </a>
+            </li>
+                        <li><a href=".?category_id=3">
+                    Drums                </a>
+            </li>
+                    </ul>
         </nav>          
     </aside>
 
     <section>
         <!-- display a table of products -->
-        <h2><?php echo $category_name; ?></h2>
+        <h2>Guitars</h2>
         <table>
             <tr>
                 <th>Code</th>
@@ -79,27 +43,85 @@ $statement3->closeCursor();
                 <th>&nbsp;</th>
             </tr>
 
-            <?php foreach ($products as $product) : ?>
-            <tr>
-                <td><?php echo $product['productCode']; ?></td>
-                <td><?php echo $product['productName']; ?></td>
-                <td class="right"><?php echo $product['listPrice']; ?></td>
+                        <tr>
+                <td>strat</td>
+                <td>Fender Stratocaster</td>
+                <td class="right">699.00</td>
                 <td><form action="delete_product.php" method="post">
                     <input type="hidden" name="product_id"
-                           value="<?php echo $product['productID']; ?>">
+                           value="1">
                     <input type="hidden" name="category_id"
-                           value="<?php echo $product['categoryID']; ?>">
+                           value="1">
                     <input type="submit" value="Delete">
                 </form></td>
             </tr>
-            <?php endforeach; ?>
-        </table>
+                        <tr>
+                <td>les_paul</td>
+                <td>Gibson Les Paul</td>
+                <td class="right">1199.00</td>
+                <td><form action="delete_product.php" method="post">
+                    <input type="hidden" name="product_id"
+                           value="2">
+                    <input type="hidden" name="category_id"
+                           value="1">
+                    <input type="submit" value="Delete">
+                </form></td>
+            </tr>
+                        <tr>
+                <td>sg</td>
+                <td>Gibson SG</td>
+                <td class="right">2517.00</td>
+                <td><form action="delete_product.php" method="post">
+                    <input type="hidden" name="product_id"
+                           value="3">
+                    <input type="hidden" name="category_id"
+                           value="1">
+                    <input type="submit" value="Delete">
+                </form></td>
+            </tr>
+                        <tr>
+                <td>fg700s</td>
+                <td>Yamaha FG700S</td>
+                <td class="right">489.99</td>
+                <td><form action="delete_product.php" method="post">
+                    <input type="hidden" name="product_id"
+                           value="4">
+                    <input type="hidden" name="category_id"
+                           value="1">
+                    <input type="submit" value="Delete">
+                </form></td>
+            </tr>
+                        <tr>
+                <td>washburn</td>
+                <td>Washburn D10S</td>
+                <td class="right">299.00</td>
+                <td><form action="delete_product.php" method="post">
+                    <input type="hidden" name="product_id"
+                           value="5">
+                    <input type="hidden" name="category_id"
+                           value="1">
+                    <input type="submit" value="Delete">
+                </form></td>
+            </tr>
+                        <tr>
+                <td>rodriguez</td>
+                <td>Rodriguez Caballero 11</td>
+                <td class="right">415.00</td>
+                <td><form action="delete_product.php" method="post">
+                    <input type="hidden" name="product_id"
+                           value="6">
+                    <input type="hidden" name="category_id"
+                           value="1">
+                    <input type="submit" value="Delete">
+                </form></td>
+            </tr>
+                    </table>
         <p><a href="add_product_form.php">Add Product</a></p>
         <p><a href="category_list.php">List Categories</a></p>        
     </section>
 </main>
 <footer>
-    <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
+    <p>&copy; 2017 My Guitar Shop, Inc.</p>
 </footer>
 </body>
 </html>
